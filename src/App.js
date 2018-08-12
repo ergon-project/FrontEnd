e35e69c489a150876d81ad98c10af2727a55f230
Alex Loaiza <zer0dev@localhost.localdomain>
2018-08-12 04:24:17 -0700

    Resolved main layout conflicts

--- src/App.js
+++ src/App.js
@@ -2,31 +2,25 @@ import React, { Component } from "react";
 import "./App.css";
 import { Button } from "react-materialize";
 import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
-// import MainLayout from './components/layout/MainLayout';
-import AppNavbar from "./components/layout/AppNavbar";
-import Signup from "./components/Signup";
-// import { Link } from "react-router-dom";
-// import Login fom "./components/Login";
+import MainLayout from './components/layout/MainLayout';
 
 class App extends Component {
 	render() {
 		return (
 			<Router>
-			<div className="App">
-				<AppNavbar />
-				<div className="container">
-					<Switch>
-				{/* <Route exact path='/' render={() => { */}
-					{/* // return  */}
-				
-				{/* // }} /> */}
-				 <Route exact path="/register" component={Signup}/>
-				 {/* <Route exact path="/login" component={Login}/> */}
-				
-				
-
-				 {/* <Route
-				 	exact
+			<Switch>
+				<Route exact path='/' render={() => {
+					return <MainLayout />
+				}} />
+				<Route
+					exact
+					path="/register"
+					render={() => {
+						// return <SignupPage />;
+					}}
+				/>
+				<Route
+					exact
 					path="/login"
 					render={() => {
 						// return <LoginPage />;
@@ -39,15 +33,16 @@ class App extends Component {
 						// return <LoginPage />;
 					}}
 				/>
-				<Route exact path="/events/:eventId" render={({ match }) => {
-					const id = match.params.eventId;
+				<Route
+					exact
+					path="/events/:eventId"
+					render={({ match }) => {
+						const id = match.params.eventId;
 						// return <EventPage eventId={id} />;
 					}}
-				/> */}
+				/>
 			</Switch>
-            </div>
-          </div>
-        </Router>
+			</Router>
 		);
 	}
 }
