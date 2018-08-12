95ab4a488b23286ab80956d03c5c030ff652b61e
Alex Loaiza <zer0dev@localhost.localdomain>
2018-08-12 04:48:39 -0700

    calendar fix

---
+++ src/components/Cards.js
@@ -0,0 +1,115 @@
+import React, { Component } from "react";
+import {Row, Col, Card, CardTitle} from "react-materialize";
+
+class MyCard extends Component {
+	render() {
+		return (
+            <div>
+                <Row>
+                    <Col s={12} m={4} l={3}>
+                    <div class="card">
+                        <div className="card-image waves-effect waves-block waves-light">
+                        <img className="activator" src="https://www.developmentafrica.net/wp-content/uploads/2016/09/Upcoming-Events.jpeg"/>
+                        </div>
+                        <div className="card-content">
+                        <span className="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+                        <p><a href="#">This is a link</a></p>
+                        </div>
+                        <div className="card-reveal">
+                                <div class="card__meta">
+                                    <a href="#"><i class="small material-icons">room</i>Los Gatos, Ca </a>
+                                </div>
+                                <span class="card-title blue-text text-darken-4">STAY HACKING 2018</span>
+
+                                <p class="card-subtitle grey-text text-darken-2">Cool hackathon in netflix!</p>
+                                <img class="responsive-img map" src="https://static1.squarespace.com/static/54ff63f0e4b0bafce6932642/t/5a383ae241920241ebcd7aee/1529686973902/1-21+-+New+York.gif?format=750w"/>
+                                <span class="blue-text text-darken-2 card-info"><i class="small material-icons">label</i>&nbsp;Stayhacking, Netflix</span>
+                                <div class="card-action">
+                                <a href="#"><i class="material-icons">&nbsp;edit</i>EDIT</a>
+                                <a href="#" class="card-action-right right"><i class="material-icons">&nbsp;info</i>DELETE</a>
+                            </div>
+                        </div>
+                    </div>
+                    </Col>
+                    <Col s={12} m={4} l={3}>
+                    <div class="card">
+                        <div className="card-image waves-effect waves-block waves-light">
+                        <img className="activator" src="https://www.developmentafrica.net/wp-content/uploads/2016/09/Upcoming-Events.jpeg"/>
+                        </div>
+                        <div className="card-content">
+                        <span className="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+                        <p><a href="#">This is a link</a></p>
+                        </div>
+                        <div className="card-reveal">
+                                <div class="card__meta">
+                                    <a href="#"><i class="small material-icons">room</i>Los Gatos, Ca </a>
+                                </div>
+                                <span class="card-title blue-text text-darken-4">STAY HACKING 2018</span>
+
+                                <p class="card-subtitle grey-text text-darken-2">Cool hackathon in netflix!</p>
+                                <img class="responsive-img map" src="https://static1.squarespace.com/static/54ff63f0e4b0bafce6932642/t/5a383ae241920241ebcd7aee/1529686973902/1-21+-+New+York.gif?format=750w"/>
+                                <span class="blue-text text-darken-2 card-info"><i class="small material-icons">label</i>&nbsp;Stayhacking, Netflix</span>
+                                <div class="card-action">
+                                <a href="#"><i class="material-icons">&nbsp;edit</i>EDIT</a>
+                                <a href="#" class="card-action-right right"><i class="material-icons">&nbsp;info</i>DELETE</a>
+                            </div>
+                        </div>
+                    </div>
+                    </Col>
+                    <Col s={12} m={4} l={3}>
+                    <div class="card">
+                        <div className="card-image waves-effect waves-block waves-light">
+                        <img className="activator" src="https://www.developmentafrica.net/wp-content/uploads/2016/09/Upcoming-Events.jpeg"/>
+                        </div>
+                        <div className="card-content">
+                        <span className="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+                        <p><a href="#">This is a link</a></p>
+                        </div>
+                        <div className="card-reveal">
+                                <div class="card__meta">
+                                    <a href="#"><i class="small material-icons">room</i>Los Gatos, Ca </a>
+                                </div>
+                                <span class="card-title blue-text text-darken-4">STAY HACKING 2018</span>
+
+                                <p class="card-subtitle grey-text text-darken-2">Cool hackathon in netflix!</p>
+                                <img class="responsive-img map" src="https://static1.squarespace.com/static/54ff63f0e4b0bafce6932642/t/5a383ae241920241ebcd7aee/1529686973902/1-21+-+New+York.gif?format=750w"/>
+                                <span class="blue-text text-darken-2 card-info"><i class="small material-icons">label</i>&nbsp;Stayhacking, Netflix</span>
+                                <div class="card-action">
+                                <a href="#"><i class="material-icons">&nbsp;edit</i>EDIT</a>
+                                <a href="#" class="card-action-right right"><i class="material-icons">&nbsp;info</i>DELETE</a>
+                            </div>
+                        </div>
+                    </div>
+                    </Col>
+                    <Col s={12} m={4} l={3}>
+                    <div class="card">
+                        <div className="card-image waves-effect waves-block waves-light">
+                        <img className="activator" src="https://www.developmentafrica.net/wp-content/uploads/2016/09/Upcoming-Events.jpeg"/>
+                        </div>
+                        <div className="card-content">
+                        <span className="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+                        <p><a href="#">This is a link</a></p>
+                        </div>
+                        <div className="card-reveal">
+                                <div class="card__meta">
+                                    <a href="#"><i class="small material-icons">room</i>Los Gatos, Ca </a>
+                                </div>
+                                <span class="card-title blue-text text-darken-4">STAY HACKING 2018</span>
+
+                                <p class="card-subtitle grey-text text-darken-2">Cool hackathon in netflix!</p>
+                                <img class="responsive-img map" src="https://static1.squarespace.com/static/54ff63f0e4b0bafce6932642/t/5a383ae241920241ebcd7aee/1529686973902/1-21+-+New+York.gif?format=750w"/>
+                                <span class="blue-text text-darken-2 card-info"><i class="small material-icons">label</i>&nbsp;Stayhacking, Netflix</span>
+                                <div class="card-action">
+                                <a href="#"><i class="material-icons">&nbsp;edit</i>EDIT</a>
+                                <a href="#" class="card-action-right right"><i class="material-icons">&nbsp;info</i>DELETE</a>
+                            </div>
+                        </div>
+                    </div>
+                    </Col>
+                </Row>
+            </div>
+		);
+	}
+}
+
+export default MyCard;
